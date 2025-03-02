a0cf0f405859ebcf4829e1c79d083b397882bea5
JamarNucum <nucumj14887@gotritons.uog.edu>
2025-03-02 22:55:32 +1000

    Finished Table

--- src/main/java/com/example/scenebuilder/Components.java
+++ src/main/java/com/example/scenebuilder/Components.java
@@ -57,27 +57,105 @@ public class Components {
         );
     }
  public static void updTable(int step, ObservableList<Components> tableComps, TableView<Components> tableView) {
-        switch (step) {
-            case 1:
-                tableComps.get(0).setInstruction("1");
-                break;
-            case 3:
-                tableComps.get(1).setInstruction("MAR Input here");
-                break;
-            case 5:
-                tableComps.get(2).setInstruction("MBR Input here");
-                break;
-
-            case 7:
-                tableComps.get(3).setInstruction("IR Input here");
-                break;
-            case 9:
-                tableComps.get(4).setInstruction("IBR Input here");
-                break;
-            case 11:
-                tableComps.get(5).setInstruction("AC Input here");
-                break;
-        }
-        tableView.refresh();
-    }
+     switch (step) {
+         case 1:
+             tableComps.get(0).setInstruction("1");
+             break;
+         case 2:
+             tableComps.get(1).setInstruction("1");
+             break;
+         case 3:
+             tableComps.get(1).setInstruction("1");
+             break;
+         case 4:
+             tableComps.get(2).setInstruction("LOAD M(X) 500, ADD M(X) 501");
+             break;
+         case 5:
+             tableComps.get(3).setInstruction("LOAD M(X) 500");
+             tableComps.get(4).setInstruction("ADD M(X) 501");
+             break;
+         case 6:
+             tableComps.get(1).setInstruction("500");
+             break;
+         case 7:
+             tableComps.get(1).setInstruction("500");
+             break;
+         case 8:
+             // Memory retrieval
+             break;
+         case 9:
+             tableComps.get(2).setInstruction("3");
+             break;
+         case 10:
+             tableComps.get(2).setInstruction("3");
+             break;
+         case 11:
+             tableComps.get(5).setInstruction("3");
+             break;
+         case 12:
+             tableComps.get(5).setInstruction("3");
+             break;
+         case 13:
+             tableComps.get(3).setInstruction("ADD M(X) 501");
+             tableComps.get(4).setInstruction("-");
+             break;
+         case 14:
+             tableComps.get(1).setInstruction("501");
+             break;
+         case 15:
+             tableComps.get(1).setInstruction("501");
+             break;
+         case 16:
+             tableComps.get(0).setInstruction("2");
+             break;
+         case 17:
+             // Memory retrieval
+             break;
+         case 18:
+             tableComps.get(2).setInstruction("4");
+             break;
+         case 19:
+             tableComps.get(2).setInstruction("4");
+             break;
+         case 20:
+             tableComps.get(5).setInstruction("7");
+             break;
+         case 21:
+             tableComps.get(5).setInstruction("7");
+             break;
+         case 22:
+             tableComps.get(1).setInstruction("2");
+             break;
+         case 23:
+             tableComps.get(1).setInstruction("2");
+             break;
+         case 24:
+             tableComps.get(2).setInstruction("STOR M(X) 500, Other Ins");
+             break;
+         case 25:
+             tableComps.get(3).setInstruction("STOR M(X) 500");
+             tableComps.get(4).setInstruction("Other Ins");
+             break;
+         case 26:
+             tableComps.get(1).setInstruction("500");
+             break;
+         case 27:
+             tableComps.get(1).setInstruction("500");
+             break;
+         case 28:
+             tableComps.get(2).setInstruction("7");
+             break;
+         case 29:
+             tableComps.get(2).setInstruction("7");
+             break;
+         case 30:
+             // Memory write operation
+             break;
+         case 31:
+             tableComps.get(2).setInstruction("7 stored at address 500");
+             tableComps.get(5).setInstruction("7");
+             break;
+     }
+     tableView.refresh();
+ }
 }
